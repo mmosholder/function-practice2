c1de904dd7dc25b8e9578ac8b8ca421206a4b27f
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-04-14 15:57:13 -0600

    initial exercise work

---
+++ main.js
@@ -0,0 +1,72 @@
+// 1
+var namesArr = [
+	{name: 'Luisa', age: 25},
+	{name: 'Edward', age: 30}
+];
+
+var getName = function (arr) {
+	for(var i = 0; i < arr.length; i++) {
+		console.log(arr[i].name);
+	}
+};
+
+// 2
+
+ var totalLetters = function (arr) {
+		var newArr = [];
+ 	for(var i = 0; i < arr.length; i++) {
+ 		newArr.push(arr[i]);
+ 	}
+
+ 	var results = newArr.join('');
+ 	return (results.length);
+ };
+
+// 3
+var keyValue = function (city, cityName) {
+	var newCity = {
+		city: cityName
+	};
+
+	return newCity;
+};
+
+// 4
+
+var negativeIndex = function (arr, x) {
+	return (arr.length+x);
+};
+
+
+// 5
+var removeM = function (str) {
+	var emptyArr = [];
+	var newArr = str.split('');
+	for(var i = 0; i < newArr.length; i++) {
+		if (newArr[i] != 'm') {
+			emptyArr.push(newArr[i]);
+		} 
+	}
+	return (emptyArr.join(''));
+};
+
+// 6
+var printObject = function (obj) {
+	for(var key in obj) {
+		console.log(key + ' is ' + obj[key]);
+	}
+};
+
+// 7
+var vowels = function(str) {
+	var emptyArr = [];
+	var newArr = str.split('');
+	for(var i = 0; i < newArr.length; i++) {
+		if(newArr[i] === 'a' || newArr[i] === 'e' || newArr[i] === 'i' || newArr[i] === 'o' || newArr[i] === 'u') {
+			emptyArr.push(newArr[i]);
+		}
+	}
+
+	return emptyArr;	
+
+};
